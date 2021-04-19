10de789ae5a91b027743208b85f734311475f3a9
Astin <akjolsatinov01@gmail.com>
2021-04-19 12:08:41 +0600

    'some'

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,24 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from "react";
+import { Link, Route } from "react-router-dom";
+
+const Foo = (props: any) => {
+  console.log(props.match.params.id);
+  useEffect(() => {
+    console.log("mount");
+  }, []);
+
+  return <h1>Hello</h1>;
+};
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Link to="/foo/1">1</Link>
+      <Link to="/foo/2">2</Link>
+      {/* <Route path="/foo/:id">
+        <Foo />
+      </Route> */}
+      <Route path="/foo/:id" component={Foo} />
     </div>
   );
 }
